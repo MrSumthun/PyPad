65577c1188312f11d943de5a4db6aa578af44d9e
Plaligraphy <evanac21@gmail.com>
2025-02-02 19:41:36 -0800

    Added versions and comments for readability

--- README.md
+++ README.md
@@ -3,4 +3,16 @@ Notepad in Python utilzing the Tkinter GUI framework
 
 
 ## Updates
-Still updating actively
+Still updating actively on this project
+
+
+## Goals
+* Offer fast read/write functionality
+
+* Maintain simplistic interface
+
+
+## TODOs
+* Implement a more advanced read write system
+
+* Implement Windows support (This is developed on Arch)
--- pypad.py
+++ pypad.py
@@ -1,20 +1,26 @@
 import tkinter as tk
 import sys
 
+
 window = tk.Tk()
+
+#Initializing our commonly used variables
 window.geometry("1280x720")
+program_name = "PyPad"
+version_number = "0.1"
+background_color = 'dark slate gray'
+window_bg = 'dark slate gray'
+file = open(program_name + ".txt", "r+")
 
-file = open("pypad.txt", "r+")
+label = tk.Label(window, text=program_name + "Version: " + version_number, bg=window_bg)
 
-label = tk.Label(window, text="Basic Notepad")
-label.pack()
+#Text area will always have a black background for readability
+text_widget = tk.Text(window, height=23, width=100, bg="black")
 
-text_widget = tk.Text(window, height=20, width=80)
-text_widget.pack()
 def exit():
     sys.exit(0)
 
-exit_button = tk.Button(window, text="Exit", command=exit)
+exit_button = tk.Button(window, text="Exit", command=exit, bg=background_color)
 
 def get_text():
     text = text_widget.get("1.0", "end-1c")
@@ -23,7 +29,7 @@ def get_text():
 def console_print():
     print(str(get_text()))    
 
-print_button = tk.Button(window, text="Print to Console", command=console_print)
+print_button = tk.Button(window, text="Print to Console", command=console_print, bg=background_color)
 
 def print_to_file():
     file.seek(0)
@@ -32,7 +38,7 @@ def print_to_file():
     file.flush()
     
 
-write_file_button = tk.Button(window, text="Print to File", command=print_to_file)
+write_file_button = tk.Button(window, text="Print to File", command=print_to_file, bg=background_color)
 
 def read_from_file():
     file.seek(0)
@@ -40,23 +46,28 @@ def read_from_file():
     text_widget.insert(tk.END, file_text)
     print("Read from file!")
 
-read_file_button = tk.Button(window, text="Read from file", command=read_from_file)
+read_file_button = tk.Button(window, text="Read from File", command=read_from_file, bg=background_color)
 
 def clear_text_area():
     text_widget.delete("1.0", tk.END)
 
-clear_text_button = tk.Button(window, text="Clear Text Area", command=clear_text_area)
+clear_text_button = tk.Button(window, text="Clear Text Area", command=clear_text_area, bg=background_color)
 
 def clear_file():
     file.truncate(0)
 
-clear_file_button = tk.Button(window, text="Clear File Contents", command=clear_file)
+clear_file_button = tk.Button(window, text="Clear File Contents", command=clear_file, bg=background_color)
 
+window.configure(bg=window_bg)
 
+# Sets placement of all elements
+label.pack()
+text_widget.pack()
 print_button.pack(side="left")
 write_file_button.pack(side="left")
 read_file_button.pack(side="left")
-clear_file_button.pack(side="left")
-clear_text_button.pack(side="left")
-exit_button.pack(side="left")
+clear_file_button.pack(side="right")
+clear_text_button.pack(side="right")
+exit_button.pack(side="bottom")
+
 window.mainloop()
